b5eb146bf5860287803e6d2f4ccea6e883568add
richam68 <richamhshwr2@gmail.com>
2024-08-04 23:27:41 +0530

    alert added

--- src/components/weather.js
+++ src/components/weather.js
@@ -23,6 +23,7 @@ const Weather = () => {
 
     } catch (err) {
       console.log("err", err);
+      alert("Failed to fetch weather data");
     }finally{
         setLoading(false)
     }
@@ -44,14 +45,14 @@ const Weather = () => {
       <button className="button" onClick={weatherApi}>Search</button>
 
       {loading && <p>Loading data...</p> }
-      {weatherData && 
+      {!loading && weatherData && (
       <div className="weather-cards">
         <Card heading="Temperature" data={`${weatherData.current.humidity}°C`}/>
         <Card heading="Humidity" data={`${weatherData.current.temp_c}%`}/>
         <Card heading="Condition" data={weatherData.current.condition.text}/>
         <Card heading="Wind" data={`${weatherData.current.wind_kph}kph`}/>
         </div>
-}
+)}
     </div>
   );
 };
